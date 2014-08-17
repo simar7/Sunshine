b3fe9e12f71325e46679be2c40b9c18718dbc495
Simarpreet Singh <s244sing@uwaterloo.ca>
2014-08-17 19:25:16 -0400

    Lesson 1 complete.

--- app/src/main/java/com/example/simar/sunshine/MyActivity.java
+++ app/src/main/java/com/example/simar/sunshine/MyActivity.java
@@ -10,7 +10,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MyActivity extends ActionBarActivity {
@@ -57,7 +63,27 @@ public class MyActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
+
+            // Create a rootView
             View rootView = inflater.inflate(R.layout.fragment_my, container, false);
+
+            // Add some fake data by using an ArrayList of Strings
+            ArrayList FakeForecastData = new ArrayList<String>
+                    (Arrays.asList("Today | 30C", "Tomorrow | 20C",
+                            "Day After | 10C")  );
+
+            // Create an adapter, it takes: Activity, id of the forecast, id of the forecast_textview and fakeData.
+            ArrayAdapter<String> ForecastAdapter = new ArrayAdapter<String>
+                    (getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview, FakeForecastData);
+
+            // Get the ID of the ListView by traversing the hierarchy at the rootView Node.
+            ListView listView  = (ListView) rootView.findViewById(R.id.listview_forecast);
+
+            // Bind the adapter to the ListView.
+            listView.setAdapter(ForecastAdapter);
+
             return rootView;
         }
     }
