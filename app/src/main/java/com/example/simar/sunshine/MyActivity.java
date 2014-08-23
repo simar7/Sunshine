f7a2dedf80c3437ded9135131f1d1370bf36344f
Simarpreet Singh <s244sing@uwaterloo.ca>
2014-08-23 13:44:08 -0400

    Refactoring ForecastFragment, should've auto-refac.

--- app/src/main/java/com/example/simar/sunshine/MyActivity.java
+++ app/src/main/java/com/example/simar/sunshine/MyActivity.java
@@ -1,22 +1,11 @@
 package com.example.simar.sunshine;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.ArrayAdapter;
-import android.widget.FrameLayout;
-import android.widget.ListView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MyActivity extends ActionBarActivity {
@@ -27,7 +16,7 @@ public class MyActivity extends ActionBarActivity {
         setContentView(R.layout.activity_my);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -52,39 +41,4 @@ public class MyActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-
-            // Create a rootView
-            View rootView = inflater.inflate(R.layout.fragment_my, container, false);
-
-            // Add some fake data by using an ArrayList of Strings
-            ArrayList FakeForecastData = new ArrayList<String>
-                    (Arrays.asList("Today | 30C", "Tomorrow | 20C",
-                            "Day After | 10C")  );
-
-            // Create an adapter, it takes: Activity, id of the forecast, id of the forecast_textview and fakeData.
-            ArrayAdapter<String> ForecastAdapter = new ArrayAdapter<String>
-                    (getActivity(),
-                    R.layout.list_item_forecast,
-                    R.id.list_item_forecast_textview, FakeForecastData);
-
-            // Get the ID of the ListView by traversing the hierarchy at the rootView Node.
-            ListView listView  = (ListView) rootView.findViewById(R.id.listview_forecast);
-
-            // Bind the adapter to the ListView.
-            listView.setAdapter(ForecastAdapter);
-
-            return rootView;
-        }
-    }
 }
